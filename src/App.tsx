8005622c6e20887aca7013a0c00bc51fcb9690ec
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-07-05 11:06:48 -0600

    point to aquifer-server and deploy to Azure (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [html, setHtml] = useState('');
+  useEffect(() => {
+    fetch('https://kyletesting.azurewebsites.net/')
+      .then((r) => r.text())
+      .then((t) => setHtml(t));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
+        {html}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
