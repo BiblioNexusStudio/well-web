84deb0bc155c7b8b54d0d101f295bc1d65f70768
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2023-08-08 11:39:30 -0400

    bib-22-set-up-application-insights (#23)
    * Add logger and move config to store
    
    * Update yarn lock
    
    * Add env to logs
    
    * Remove top level await

---
+++ src/lib/logger.ts
@@ -0,0 +1,38 @@
+﻿import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { config } from '$lib/stores/config.store';
+import type { Configuration } from '$lib/stores/config.store';
+
+let configuration: Configuration;
+let appInsights: ApplicationInsights;
+let additionalProperties = {};
+
+config.subscribe((value) => {
+    if (!value) return;
+
+    configuration = value;
+
+    appInsights = new ApplicationInsights({
+        config: {
+            connectionString: configuration.APPLICATION_INSIGHTS.CONNECTION_STRING,
+        },
+    });
+
+    appInsights.loadAppInsights();
+
+    additionalProperties = {
+        source: 'aquifer-web',
+        environment: configuration.ENV,
+    };
+});
+
+export const log = {
+    exception: (ex: any) => {
+        appInsights.trackException(ex, additionalProperties);
+    },
+    pageView: (routeId: string) => {
+        appInsights.trackPageView({
+            name: routeId,
+            properties: additionalProperties,
+        });
+    },
+};
