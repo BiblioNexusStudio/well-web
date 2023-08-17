fceb6544bd07c9f4c63866eb971e3db41be37d43
Carlos Morales <carlos.morales@biblionexus.org>
2023-08-17 13:37:21 -0400

    wiredFooterMethods

--- src/lib/components/file-manager/Footer.svelte
+++ src/lib/components/file-manager/Footer.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
     import { _ as translate } from 'svelte-i18n';
+    import { currentBibleBook } from '$lib/stores/file-manager.store';
+
+    $: console.log('currentBibleBook', $currentBibleBook);
+
+    const updateFiles = () => {
+        console.log('updateFiles');
+    };
+
+    const cancelUpdateFiles = () => {
+        console.log('cancelUpdateFiles');
+    };
 </script>
 
 <footer class="footer footer-center p-4 bg-base-300 text-base-content fixed bottom-0 left-0">
@@ -10,8 +21,12 @@
             >
         </div>
         <div class="flex">
-            <button class="btn btn-neutral">{$translate('page.index.cancel.value')}</button>
-            <button class="btn btn-primary ml-4">{$translate('page.index.update.value')}</button>
+            <button class="btn btn-neutral" on:click={updateFiles}
+                >{$translate('page.index.cancel.value')}</button
+            >
+            <button class="btn btn-primary ml-4" on:click={cancelUpdateFiles}
+                >{$translate('page.index.update.value')}</button
+            >
         </div>
     </div>
 </footer>
