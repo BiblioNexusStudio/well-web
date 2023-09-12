309c0dff33bfb29eed45cfd562d38d370d19ae9b
Carlos Morales <carlos.morales@biblionexus.org>
2023-09-12 14:15:34 -0400

    logoPlaceholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,15 @@
 
 <section class="container mx-auto flex h-screen">
     <div class="flex flex-col flex-grow self-center">
-        <h1 class="text-center font-semibold text-primary text-7xl pb-4">AQUIFER</h1>
+        <div class="flex w-full max-w-xs mx-auto px-6 pb-4">
+            <div id="bible-well-logo" class="flex w-16 h-auto mr-6">
+                <img src="/aquifer-logo.svg" alt="Bible Well Logo" />
+            </div>
+            <div class="flex flex-col">
+                <div class="text-left text-primary text-2xl pb-2">The</div>
+                <h1 class="text-left font-semibold text-primary text-4xl pb-2">Bible Well</h1>
+            </div>
+        </div>
         <h2 class="text-center font-semibold text-slate-400 text-xl pb-4 max-w-xs mx-auto">
             {$translate('page.index.subTitle.value')}
         </h2>
