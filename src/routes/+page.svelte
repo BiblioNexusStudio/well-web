50829d0245f0ef6ec65adc35770e44377f29a093
Carlos Morales <carlos.morales@biblionexus.org>
2023-09-12 13:41:19 -0400

    updatesToHomePage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { _ as translate } from 'svelte-i18n';
     import { goto } from '$app/navigation';
     import PassageForm from '$lib/components/PassageForm.svelte';
+    import { isOnline } from '$lib/stores/is-online.store';
 
     const goToFileManager = () => {
         goto('/file-manager');
@@ -9,11 +10,16 @@
 </script>
 
 <section class="container mx-auto flex h-screen">
-    <div class="flex-grow self-center">
-        <h1 class="text-center font-semibold text-primary text-7xl pb-6">AQUIFER</h1>
+    <div class="flex flex-col flex-grow self-center">
+        <h1 class="text-center font-semibold text-primary text-7xl pb-4">AQUIFER</h1>
+        <h2 class="text-center font-semibold text-slate-400 text-xl pb-4 max-w-xs mx-auto">
+            {$translate('page.index.subTitle.value')}
+        </h2>
         <PassageForm />
-        <button class="btn btn-info mx-auto mt-8 mx-auto" on:click|preventDefault={goToFileManager}
-            >{$translate('page.index.downloadResourcesForOfflineUse.value')}</button
+        <button class="mt-6 mx-auto text-sky-500 max-w-xs" on:click|preventDefault={goToFileManager}
+            >{$isOnline
+                ? $translate('page.index.downloadResourcesForOfflineUse.value')
+                : $translate('sideMenu.fileManager.value')}</button
         >
     </div>
 </section>
