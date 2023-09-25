11ddfd142ab42c2de0fbaeb2958048f9d5315561
Carlos Morales <carlos.morales@biblionexus.org>
2023-09-25 09:39:44 -0400

    viewTableStylingDoneWithSomeLogicForChangeMenu

--- src/lib/components/file-manager/Table.svelte
+++ src/lib/components/file-manager/Table.svelte
@@ -2,10 +2,16 @@
     import { tableType } from '$lib/stores/file-manager.store';
     import BibleTable from '$lib/components/file-manager/BibleTable.svelte';
     import ResourcesTable from '$lib/components/file-manager/ResourcesTable.svelte';
+    import { featureFlags } from '$lib/stores/feature-flags.store';
+    import ViewTable from './ViewTable.svelte';
 </script>
 
-{#if $tableType === 'bible'}
-    <BibleTable />
+{#if !$featureFlags.newFileManager}
+    {#if $tableType === 'bible'}
+        <BibleTable />
+    {:else}
+        <ResourcesTable />
+    {/if}
 {:else}
-    <ResourcesTable />
+    <ViewTable />
 {/if}
