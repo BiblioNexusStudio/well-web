45faa5e0e99bd1daa567b306733511b5f29113c3
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2023-10-25 10:19:14 -0400

    Replace icon logo with text logo (#116)
    * Replace index "Bible Well" text with logo that includes text
    
    * Remove extra div around logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,8 @@
 <section class="container mx-auto flex h-screen flex-col">
     <div class="flex-[2]" />
     <div class="flex flex-grow flex-col self-center">
-        <div class="mx-auto flex w-full max-w-xs px-6 pb-4">
-            <div id="bible-well-logo" class="mr-6 flex h-auto w-16">
-                <img class="object-contain" src="/logo-192.png" alt="Bible Well Logo" />
-            </div>
-            <div class="flex flex-col">
-                <div class="pb-2 text-left text-2xl text-primary">The</div>
-                <h1 class="pb-2 text-left text-4xl font-semibold text-primary">Bible Well</h1>
-            </div>
+        <div id="bible-well-logo" class="mx-auto flex h-auto w-full max-w-xs px-6 pb-4">
+            <img class="object-contain" src="/bibleWellLogoWithText-568.png" alt="Bible Well Logo" />
         </div>
         <h2 class="mx-auto max-w-xs pb-4 text-center text-xl font-semibold text-slate-400">
             {$translate('page.index.subTitle.value')}
