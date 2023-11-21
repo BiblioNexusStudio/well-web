88b502be2e29006061eff50ddcfd717daea69cfe
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-11-21 10:26:33 -0700

    update error message and include button for clearing cache

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,11 @@
     import { onMount } from 'svelte';
     import { page } from '$app/stores';
     import { log } from '$lib/logger';
-    import { updateOnlineStatus } from '$lib/stores/is-online.store';
+    import { isOnline, updateOnlineStatus } from '$lib/stores/is-online.store';
     import { featureFlags } from '$lib/stores/feature-flags.store';
     import DebugModal from '$lib/components/DebugModal.svelte';
     import { fetchFromCacheOrApi } from '$lib/data-cache';
+    import { clearEntireCache } from '$lib/data-cache';
     import type { LayoutData } from './$types';
 
     $: log.pageView($page.route.id ?? '');
@@ -74,12 +75,18 @@
 <DebugModal />
 
 {#if data.error}
-    <div class="flex h-full w-full items-center">
+    <div class="mx-auto flex h-full w-full max-w-lg flex-col items-center space-y-4 px-4 text-lg">
         <div class="flex-grow"></div>
         <!-- can't be translated since the translation strings failed to load -->
-        <div class="text-lg">
-            Fatal Error: Try refreshing. If you continue to see this error, try clearing your cache.
-        </div>
+        <div class="text-center">An error occurred. Please try refreshing your browser.</div>
+        {#if $isOnline}
+            <div class="text-center">
+                If this error persists, please clear your cache by clicking the button below. <b
+                    >Warning! Clearing the cache will delete all downloaded content.</b
+                >
+            </div>
+            <button class="btn" on:click={clearEntireCache}>Clear Cache</button>
+        {/if}
         <div class="flex-grow"></div>
     </div>
 {:else}
