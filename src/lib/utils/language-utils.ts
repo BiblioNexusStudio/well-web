bbef5765047eb46ea1f8074aa4cd20bf6a98732b
Carlos Morales <carlos.rey.morales@gmail.com>
2023-12-01 10:02:20 -0500

    directionalSupportedAddedAndDebugMode (#146)
    * directionalSupportedAddedAndDebugMode
    
    * madeRequestedPrChanges

--- src/lib/utils/language-utils.ts
+++ src/lib/utils/language-utils.ts
@@ -1,12 +1,14 @@
 export const LanguageCode = { ENG: 'eng', HIN: 'hin', TPI: 'tpi' } as const;
+export const DirectionCode = { LTR: 'ltr', RTL: 'rtl' } as const;
 
 export type LanguageCodeEnum = (typeof LanguageCode)[keyof typeof LanguageCode];
+export type DirectionCodeEnum = (typeof DirectionCode)[keyof typeof DirectionCode];
 
 export const supportedLanguages = [
-    { code: LanguageCode.ENG, label: 'English' },
-    { code: LanguageCode.HIN, label: 'हिंदी' },
-    { code: LanguageCode.TPI, label: 'Tok Pisin' },
-] as { code: LanguageCodeEnum; label: string }[];
+    { code: LanguageCode.ENG, label: 'English', direction: DirectionCode.LTR },
+    { code: LanguageCode.HIN, label: 'हिंदी', direction: DirectionCode.LTR },
+    { code: LanguageCode.TPI, label: 'Tok Pisin', direction: DirectionCode.LTR },
+] as { code: LanguageCodeEnum; label: string; direction: DirectionCodeEnum }[];
 
 export function browserLanguageToISO6393(browserLanguage: string) {
     const twoDigit = browserLanguage.toLowerCase().split('-')[0];
