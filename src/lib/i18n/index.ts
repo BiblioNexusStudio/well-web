0c124386461a69ba82773bbcbdc582359207f108
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-01-26 14:38:30 -0700

    add french and russian to Well

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -2,6 +2,8 @@ import { init as svelteI18nInit, register, locale } from 'svelte-i18n';
 
 register('eng', () => import('./locales/eng.json'));
 register('hin', () => import('./locales/hin.json'));
+register('fra', () => import('./locales/fra.json'));
+register('rus', () => import('./locales/rus.json'));
 register('tpi', () => import('./locales/tpi.json'));
 register('arb', () => import('./locales/arb.json'));
 register('swh', () => import('./locales/swh.json'));
