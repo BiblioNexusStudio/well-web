dfd84bbed339eb9fff2006599c049f214079eb7b
Carlos Morales <carlos.morales@biblionexus.org>
2024-04-01 16:27:21 -0400

    fixingMenuSwishImage

--- src/lib/components/MenuPage.svelte
+++ src/lib/components/MenuPage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import { _ as translate } from 'svelte-i18n';
-    import menuSwish from '$lib/assets/menu-swish.png';
     import DownloadIcon from '$lib/icons/DownloadIcon.svelte';
     import HomeIcon from '$lib/icons/HomeIcon.svelte';
     import LanguageIcon from '$lib/icons/LanguageIcon.svelte';
@@ -10,7 +9,7 @@
 <div class="z-50 flex h-full w-full flex-col">
     <div class="relative mb-6 flex h-[166px] w-full rounded-b-3xl bg-[#00A3E0] pl-6 pt-12">
         <div class="absolute bottom-0 left-0 w-full">
-            <img src={menuSwish} alt="Menu Swish" class="h-auto w-full rounded-b-3xl" />
+            <img src="/src/lib/assets/menu-swish.png" alt="Menu Swish" class="h-auto w-full rounded-b-3xl" />
         </div>
         <h2 class="z-10 text-2xl font-bold text-white">{$translate('page.menu.menu.value')}</h2>
     </div>
