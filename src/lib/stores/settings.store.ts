e6a01841f8841add76866cd2bd4fb53715ddad22
Carlos Morales <carlos.morales@biblionexus.org>
2024-05-03 11:12:16 -0400

    settingsNowSavingLocallyAndSetOnRefresh

--- src/lib/stores/settings.store.ts
+++ src/lib/stores/settings.store.ts
@@ -1,5 +1,8 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 import { type Setting, SettingShortNameEnum, ParentResourceIdEnum } from '../types/settings';
+
+const bibleWellSettingsInLocalStorage = 'BIBLE_WELL_SETTINGS_IN_LOCAL_STORAGE';
 
 const showOnlySrvResources: Setting = {
     text: 'Show Only SRV Resources',
@@ -8,4 +11,23 @@ const showOnlySrvResources: Setting = {
     parentResourceIds: [ParentResourceIdEnum.CBBTER, ParentResourceIdEnum.VideoBibleDictionary],
 };
 
-export const settings = writable<Setting[]>([showOnlySrvResources]);
+const defaultSettings = [showOnlySrvResources];
+
+let savedSettings: Setting[] = [];
+
+if (browser) {
+    savedSettings = JSON.parse(localStorage.getItem(bibleWellSettingsInLocalStorage) || '[]');
+}
+
+const initialSettings = defaultSettings.map((setting) => {
+    const savedSetting = savedSettings.find((savedSetting: Setting) => savedSetting.shortName === setting.shortName);
+    return savedSetting || setting;
+});
+
+export const settings = writable<Setting[]>(initialSettings);
+
+settings.subscribe((value) => {
+    if (browser) {
+        localStorage.setItem(bibleWellSettingsInLocalStorage, JSON.stringify(value));
+    }
+});
