887a30b827c5872eb7d6a47e6882e92e58484208
Carlos Morales <carlos.morales@biblionexus.org>
2024-05-20 09:22:39 -0400

    fixingLintIssue

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -4,7 +4,6 @@ import { browserSupported } from './utils/browser';
 import { browser } from '$app/environment';
 import type { WellFetchError } from './data-cache';
 
-
 const appInsights = new ApplicationInsights({
     config: {
         connectionString: config.PUBLIC_APPLICATION_INSIGHTS_CONNECTION_STRING,
