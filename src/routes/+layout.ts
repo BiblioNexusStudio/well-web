e404ddb715da45cd2b0605831e3d4154d5736d25
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-06-19 08:20:05 -0600

    immediately refresh when a new service worker is detected

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -19,17 +19,11 @@ export const ssr = false;
 
 export const load: LayoutLoad = async () => {
     try {
-        const initializedAt = Date.now();
         const updateServiceWorker = registerSW({
             onNeedRefresh() {
-                // if we receive the "SW update" event within 1.5s of the time the app was initialized,
-                // we know that the page must have just been refreshed, and we can go ahead and do the
-                // service worker update without fear of disturbing the user
-                if (Date.now() - initializedAt < 1500) {
-                    // The timeout is here to make the browser happy. For some reason if immediately fired, the new service
-                    // worker won't be activated.
-                    setTimeout(updateServiceWorker, 750);
-                }
+                // The timeout is here to make the browser happy. For some reason if immediately fired, the new service
+                // worker won't be activated.
+                setTimeout(updateServiceWorker, 750);
             },
         });
 
