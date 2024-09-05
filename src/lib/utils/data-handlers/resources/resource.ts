4963b4b1df4eb56c10be84dca1cc4738939c23b5
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-05 09:22:08 -0600

    add audio player for text resources with audio versions (#302)

--- src/lib/utils/data-handlers/resources/resource.ts
+++ src/lib/utils/data-handlers/resources/resource.ts
@@ -36,7 +36,7 @@ interface ResourceContentInfoForPath {
     version?: number;
 }
 
-function resourceContentApiPath(resourceContent: ResourceContentInfoForPath) {
+export function resourceContentApiPath(resourceContent: ResourceContentInfoForPath) {
     const mediaType = 'mediaType' in resourceContent ? resourceContent.mediaType : resourceContent.mediaTypeName;
     const id = 'id' in resourceContent ? resourceContent.id : resourceContent.contentId;
     if (mediaType === MediaType.Audio) {
