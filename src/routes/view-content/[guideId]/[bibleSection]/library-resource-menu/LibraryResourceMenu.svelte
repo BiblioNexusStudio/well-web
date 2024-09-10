99dbe4ba176d806b3e66c8b6263d56b87713759e
Carlos Morales <carlos.morales@biblionexus.org>
2024-09-10 10:53:27 -0400

    searchNowGoesOutToApiForMatchingResources

--- src/routes/view-content/[guideId]/[bibleSection]/library-resource-menu/LibraryResourceMenu.svelte
+++ src/routes/view-content/[guideId]/[bibleSection]/library-resource-menu/LibraryResourceMenu.svelte
@@ -250,14 +250,21 @@
         setIsResourceSearch(true);
     }
 
-    async function searchByParentResource(parentResource: ApiParentResource, offset: number | null) {
+    async function searchByParentResource(
+        parentResource: ApiParentResource | null,
+        offset: number | null,
+        query: string = ''
+    ) {
+        if (!parentResource) return;
+        if (query.length === 1 || query.length === 2) return;
+
         isLoading = true;
         const currentLanguageId = get(currentLanguageInfo)?.id;
         let response = (await fetchFromCacheOrApi(
             ...searchResourcesEndpoint(
                 currentLanguageId,
-                '',
-                [parentResource.resourceType],
+                query,
+                [parentResource?.resourceType],
                 '',
                 0,
                 0,
@@ -268,7 +275,11 @@
             )
         )) as ResourceContentInfo[];
 
-        resourceSearchResources = [...resourceSearchResources, ...response];
+        if (query.length === 0) {
+            resourceSearchResources = [...resourceSearchResources, ...response];
+        } else if (query.length >= 3 && response.length > 0) {
+            resourceSearchResources = response;
+        }
     }
 </script>
 
