745009b9c54be71c34a0f056ed15670081a1049d
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-19 15:10:31 -0600

    add Portuguese and Indonesian language and update translation script

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,13 +1,15 @@
 import { init as svelteI18nInit, register, locale } from 'svelte-i18n';
 
-register('eng', () => import('./locales/eng.json'));
-register('hin', () => import('./locales/hin.json'));
-register('fra', () => import('./locales/fra.json'));
-register('rus', () => import('./locales/rus.json'));
-register('tpi', () => import('./locales/tpi.json'));
-register('swh', () => import('./locales/swh.json'));
 register('arb', () => import('./locales/arb.json'));
+register('eng', () => import('./locales/eng.json'));
+register('fra', () => import('./locales/fra.json'));
+register('hin', () => import('./locales/hin.json'));
+register('ind', () => import('./locales/ind.json'));
+register('por', () => import('./locales/por.json'));
+register('rus', () => import('./locales/rus.json'));
 register('spa', () => import('./locales/spa.json'));
+register('swh', () => import('./locales/swh.json'));
+register('tpi', () => import('./locales/tpi.json'));
 
 export async function init(currentLanguageCode: string | undefined) {
     locale.set(currentLanguageCode);
