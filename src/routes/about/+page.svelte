04fadd93831fd743c0a87554588d34ac62b46ba1
Elisabeth <shakespearee04@gmail.com>
2024-10-17 10:54:49 -0400

    add adaptations of licenses

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,8 +11,12 @@
     import { biblesEndpoint, biblesWithRestrictionsEndpoint, parentResourcesEndpoint } from '$lib/api-endpoints';
     import { filterBoolean } from '$lib/utils/array';
     import PublicDomainIcon from '$lib/icons/PublicDomainIcon.svelte';
+    import { supportedLanguages } from '$lib/stores/language.store';
 
     let licenseInfosPromise: Promise<ApiLicenseInfo[]> | null = null;
+
+    let publicDomainMark =
+        '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6 me-[-4px] mb-1 ms-[-4px] scale-[55%]" style="display:inline;" viewBox="0 0 496 512"><path d="M248 8C111 8 0 119.1 0 256c0 137 111 248 248 248s248-111 248-248C496 119.1 385 8 248 8zm0 449.5c-139.2 0-235.8-138-190.2-267.9l78.8 35.1c-2.1 10.5-3.3 21.5-3.3 32.9 0 99 73.9 126.9 120.4 126.9 22.9 0 53.5-6.7 79.4-29.5L297 311.1c-5.5 6.3-17.6 16.7-36.3 16.7-37.8 0-53.7-39.9-53.9-71.9 230.4 102.6 216.5 96.5 217.9 96.8-34.3 62.4-100.6 104.8-176.7 104.8zm194.2-150l-224-100c18.8-34 54.9-30.7 74.7-11l40.4-41.6c-27.1-23.3-58-27.5-78.1-27.5-47.4 0-80.9 20.5-100.7 51.6l-74.9-33.4c36.1-54.9 98.1-91.2 168.5-91.2 111.1 0 201.5 90.4 201.5 201.5 0 18-2.4 35.4-6.8 52-.3-.1-.4-.2-.6-.4z"/></svg>';
 
     onMount(() => {
         licenseInfosPromise = fetchLicenses();
@@ -45,6 +49,22 @@
                 },
             });
         }
+    }
+
+    function calculateAdaptedLicenseDescription(licenseInfo: ApiLicenseInfo) {
+        return $translate('page.about.license.adaptedInLanguages.value', {
+            values: {
+                title: licenseInfo.title,
+                languages: $supportedLanguages.map((language) => language.displayName).join(', '),
+                copyright:
+                    licenseInfo.copyright.holder.name.replaceAll(' ', '').toLowerCase() === 'publicdomain'
+                        ? publicDomainMark
+                        : '<span>©</span>',
+                date: licenseInfo.copyright.dates ? licenseInfo.copyright.dates : '',
+                publisher: licenseInfo.copyright.holder.name,
+                bnLicense: 'CC BY-SA 4.0',
+            },
+        });
     }
 
     function getOtherLicenses() {
@@ -145,6 +165,9 @@
                                     {@html licenseDescription}
                                 </div>
                             {/if}
+                        </div>
+                        <div class="ms-4 mt-4">
+                            {@html calculateAdaptedLicenseDescription(licenseInfo)}
                         </div>
                     {/each}
                 {:else}
